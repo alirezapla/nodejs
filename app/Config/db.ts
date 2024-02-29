e05fd6d9dc57a9174dc97d4b55a522fb4a1ba6bd
alirezapla <hajlialireza@gmail.com>
2024-03-01 02:07:50 +0330

    fix: update connectionString

--- app/Config/db.ts
+++ app/Config/db.ts
@@ -10,16 +10,15 @@ const PASSWD = process.env.DB_PASSWORD
 const DB = process.env.DB_NAME
 const HOST = process.env.DB_HOST
 
+const connectionString = `mongodb://${USERNAME}:${PASSWD}@${HOST}:27017/${DB}?authSource=admin`
+const options = {
+    autoIndex: false,
+    maxPoolSize: 100,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000, //ms(4.5 seconds)
+    family: 4
+};
 export async function dbConnect() {
-    const connectionString = `mongodb://${USERNAME}:${PASSWD}@${DB}:27017/${HOST}?authSource=admin`
-
-    const options = {
-        autoIndex: false,
-        maxPoolSize: 100,
-        serverSelectionTimeoutMS: 5000,
-        socketTimeoutMS: 45000, //ms(4.5 seconds)
-        family: 4
-    };
     try {
         const res = await mongoose.connect(connectionString, options)
         if (res) {
@@ -34,7 +33,7 @@ export async function dbConnect() {
 // export const db = mongoose.connect(connectionString, options)
 //     .then(res => {
 //         if (res) {
-//             logger.info(`succeffully connected to ${dbName}`, { event: ServiceEvents.CONNECTED_TO_DB })
+//             logger.info(`succeffully connected to ${DB}`, { event: ServiceEvents.CONNECTED_TO_DB })
 //         }
 
 //     }).catch(err => {
